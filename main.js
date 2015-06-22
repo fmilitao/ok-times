cc0b6e3f668695f3b8523cdaeb211f0400e98aed
Filipe <fm2097ad@gmail.com>
2015-06-22 18:48:36 +0100

    moved stuff around and fixed bug

--- main.js
+++ main.js
@@ -1,48 +1,78 @@
-//TODO incomplete.
-var WEIGHTS = [
-    1, 1, 1, 1, 1, 1, 1, 10, 1, 1, 1,
-    1, 1, 10, 1, 30, 30, 30, 30, 1, 1, 10,
-    1, 1, 20, 1, 40, 40, 60, 40, 1, 1, 40,
-    1, 1, 1, 1, 1, 10, 10, 10, 1, 1, 10,
-    1, 1, 10, 40, 10, 50, 70, 1, 80, 1, 10,
-    1, 1, 10, 40, 10, 50, 70, 1, 80, 1, 10,
-    1, 1, 10, 40, 10, 50, 70, 1, 80, 1, 10,
-    1, 1, 10, 40, 10, 50, 70, 1, 80, 1, 10,
-    1, 1, 10, 40, 10, 50, 70, 1, 80, 1, 10,
-    1, 1, 10, 40, 10, 50, 70, 1, 80, 1, 10,
-    1, 1, 10, 40, 10, 50, 70, 1, 80, 1, 80,
-];
-var MAX = WEIGHTS.reduce(function (old, curr) { return old + curr; }, 0);
-console.log(MAX);
-function question() {
-    var rd = Math.floor(MAX * Math.random());
-    var accum = 0;
-    for (var i_1 = 0; i_1 < WEIGHTS.length; ++i_1) {
-        accum += WEIGHTS[i_1];
-        if (accum >= rd)
-            return [Math.floor(i_1 / 11) + 2, (i_1 % 11) + 2];
+var RandomQuestion;
+(function (RandomQuestion) {
+    //TODO incomplete.
+    var WEIGHTS = [
+        1, 1, 1, 1, 1, 1, 1, 10, 1, 1, 1,
+        1, 1, 10, 1, 30, 30, 30, 30, 1, 1, 10,
+        1, 1, 20, 1, 40, 40, 60, 40, 1, 1, 40,
+        1, 1, 1, 1, 1, 10, 10, 10, 1, 1, 10,
+        1, 1, 10, 40, 10, 50, 70, 1, 80, 1, 10,
+        1, 1, 10, 40, 10, 50, 70, 1, 80, 1, 10,
+        1, 1, 10, 40, 10, 50, 70, 1, 80, 1, 10,
+        1, 1, 10, 40, 10, 50, 70, 1, 80, 1, 10,
+        1, 1, 10, 40, 10, 50, 70, 1, 80, 1, 10,
+        1, 1, 10, 40, 10, 50, 70, 1, 80, 1, 10,
+        1, 1, 10, 40, 10, 50, 70, 1, 80, 1, 80,
+    ];
+    var MAX = WEIGHTS.reduce(function (old, curr) { return old + curr; }, 0);
+    function ask() {
+        var rd = Math.floor(MAX * Math.random());
+        var accum = 0;
+        for (var i = 0; i < WEIGHTS.length; ++i) {
+            accum += WEIGHTS[i];
+            if (accum >= rd)
+                return [Math.floor(i / 11) + 2, (i % 11) + 2];
+        }
+        throw ('Error: did not find question.' + rd + ' ' + MAX + ' ' + accum);
     }
-    throw ('Error: did not find question.' + rd + ' ' + MAX + ' ' + accum);
-}
+    RandomQuestion.ask = ask;
+    ;
+})(RandomQuestion || (RandomQuestion = {}));
 ;
-//
-// TESTING
-//
-var tmp = {};
-var i = 10000;
-while (i-- > 0) {
-    var _a = question(), x = _a[0], y = _a[1];
-    var m = x + '*' + y;
-    tmp[m] = tmp[m] === undefined ? 0 : tmp[m] + 1;
-}
-for (var i_2 = 2; i_2 <= 12; i_2++) {
-    var s = '';
-    for (var j = 2; j <= 12; j++) {
-        var v = tmp[i_2 + '*' + j];
-        if (v === undefined)
-            s += '\t' + 0;
-        else
-            s += '\t' + v;
+var SequentialQuestion;
+(function (SequentialQuestion) {
+    var aux = 0;
+    function ask() {
+        var i = aux; // increments to next
+        aux = (aux + 1) % (11 * 11);
+        return [Math.floor(i / 11) + 2, (i % 11) + 2];
     }
-    console.log(s);
-}
+    SequentialQuestion.ask = ask;
+})(SequentialQuestion || (SequentialQuestion = {}));
+;
+var Test;
+(function (Test) {
+    function testRandomQuestion() {
+        var tmp = {};
+        var i = 10000;
+        while (i-- > 0) {
+            var _a = RandomQuestion.ask(), x = _a[0], y = _a[1];
+            var m = x + '*' + y;
+            tmp[m] = tmp[m] === undefined ? 0 : tmp[m] + 1;
+        }
+        for (var i_1 = 2; i_1 <= 12; i_1++) {
+            var s = '';
+            for (var j = 2; j <= 12; j++) {
+                var v = tmp[i_1 + '*' + j];
+                if (v === undefined)
+                    s += '\t' + 0;
+                else
+                    s += '\t' + v;
+            }
+            console.log(s);
+        }
+    }
+    Test.testRandomQuestion = testRandomQuestion;
+    ;
+    function testSequentialQuestion() {
+        var i = 12 * 12;
+        while (i-- > 0) {
+            console.log(SequentialQuestion.ask());
+        }
+    }
+    Test.testSequentialQuestion = testSequentialQuestion;
+    ;
+})(Test || (Test = {}));
+;
+// Test.testRandomQuestion();
+Test.testSequentialQuestion();
--- main.ts
+++ main.ts
@@ -1,59 +1,88 @@
 
-//TODO incomplete.
-const WEIGHTS = // times[x][y]
-	[ //   2,   3,   4,   5,   6,   7,   8,   9,  10,  11,  12
-		1, 1,  1,  1,  1,  1,  1, 10,  1, 1,  1, // 2
-		1, 1, 10,  1, 30, 30, 30, 30,  1, 1, 10, // 3
-		1, 1, 20,  1, 40, 40, 60, 40,  1, 1, 40, // 4
-		1, 1,  1,  1,  1, 10, 10, 10,  1, 1, 10, // 5
-		1, 1, 10, 40, 10, 50, 70,  1, 80, 1, 10, // 6
-		1, 1, 10, 40, 10, 50, 70,  1, 80, 1, 10, // 7
-		1, 1, 10, 40, 10, 50, 70,  1, 80, 1, 10, // 8
-		1, 1, 10, 40, 10, 50, 70,  1, 80, 1, 10, // 9
-		1, 1, 10, 40, 10, 50, 70,  1, 80, 1, 10, // 10
-		1, 1, 10, 40, 10, 50, 70,  1, 80, 1, 10, // 11
-		1, 1, 10, 40, 10, 50, 70,  1, 80, 1, 80, // 12
-	];
+module RandomQuestion {
+	
+	//TODO incomplete.
+	const WEIGHTS = // times[x][y]
+		[ //   2,   3,   4,   5,   6,   7,   8,   9,  10,  11,  12
+			1, 1, 1, 1, 1, 1, 1, 10, 1, 1, 1, // 2
+			1, 1, 10, 1, 30, 30, 30, 30, 1, 1, 10, // 3
+			1, 1, 20, 1, 40, 40, 60, 40, 1, 1, 40, // 4
+			1, 1, 1, 1, 1, 10, 10, 10, 1, 1, 10, // 5
+			1, 1, 10, 40, 10, 50, 70, 1, 80, 1, 10, // 6
+			1, 1, 10, 40, 10, 50, 70, 1, 80, 1, 10, // 7
+			1, 1, 10, 40, 10, 50, 70, 1, 80, 1, 10, // 8
+			1, 1, 10, 40, 10, 50, 70, 1, 80, 1, 10, // 9
+			1, 1, 10, 40, 10, 50, 70, 1, 80, 1, 10, // 10
+			1, 1, 10, 40, 10, 50, 70, 1, 80, 1, 10, // 11
+			1, 1, 10, 40, 10, 50, 70, 1, 80, 1, 80, // 12
+		];
 
+	const MAX = WEIGHTS.reduce((old, curr) => old + curr, 0);
 
-const MAX = WEIGHTS.reduce( (old, curr) => old + curr, 0);
+	export function ask(): [number, number] {
+		// random = [0, 1[
+		const rd = Math.round(MAX * Math.random());
 
-console.log(MAX);
+		let accum = 0;
+		for (let i = 0; i < WEIGHTS.length; ++i) {
+			accum += WEIGHTS[i];
+			if (accum >= rd){
+				// floor is intentional to ensure 0..11 on same 'x'
+				return [Math.floor(i / 11) + 2, (i % 11) + 2];
+			}
+		}
 
-function question() : [number,number] {
-	const rd = Math.floor(MAX*Math.random());
+		throw ('Error: did not find question.' + rd + ' ' + MAX + ' ' + accum);
+	};
 
-	let accum = 0;
-	for (let i = 0; i < WEIGHTS.length;++i){
-		accum += WEIGHTS[i];
-		if (accum >= rd)
-			return [ Math.floor(i / 11)+2, (i % 11)+2];
-	}
-
-	throw ('Error: did not find question.'+rd+' '+MAX+' '+accum);
 };
 
-//
-// TESTING
-//
+module SequentialQuestion {
+	let aux = 0;
 
-let tmp = {};
-let i = 10000;
-while( i-- > 0 ){
-	const [x, y] = question();
-	const m = x + '*' + y;
-	tmp[m] = tmp[m] === undefined ? 0 : tmp[m]+1;
-//	console.log(m);
-}
-
-for (let i = 2; i <= 12; i++){
-	let s = '';
-	for (let j = 2; j <= 12; j++ ){
-		const v = tmp[i + '*' + j];
-		if (v === undefined)
-			s += '\t' + 0;
-		else
-			s += '\t' + v;
+	export function ask () {
+		const i = aux; // increments to next
+		aux = (aux + 1) % (11 * 11);
+		return [Math.floor(i / 11) + 2, (i % 11) + 2];
 	}
-	console.log(s);
-}
+};
+
+module Test {
+
+	export function testRandomQuestion() {
+
+		let tmp = {};
+		let i = 10000;
+		while (i-- > 0) {
+			const [x, y] = RandomQuestion.ask();
+			const m = x + '*' + y;
+			tmp[m] = tmp[m] === undefined ? 0 : tmp[m] + 1;
+			//	console.log(m);
+		}
+
+		for (let i = 2; i <= 12; i++) {
+			let s = '';
+			for (let j = 2; j <= 12; j++) {
+				const v = tmp[i + '*' + j];
+				if (v === undefined)
+					s += '\t' + 0;
+				else
+					s += '\t' + v;
+			}
+			console.log(s);
+		}
+
+	};
+
+	export function testSequentialQuestion() {
+		let i = 12 * 12;
+		while (i-- > 0) {
+			console.log(SequentialQuestion.ask());
+		}
+	};
+
+};
+
+// Test.testRandomQuestion();
+// Test.testSequentialQuestion();
+
