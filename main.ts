02b6bb31aafeee882f97df853794c8cafc3db278
Filipe <fm2097ad@gmail.com>
2015-07-08 16:26:58 +0100

    additional source for weights

--- main.ts
+++ main.ts
@@ -3,6 +3,7 @@ module RandomQuestion {
 	
 	// TODO finish copying table
 	//from http://www.bbc.com/news/blogs-magazine-monitor-28143553
+	//from http://www.bbc.com/news/magazine-32299402
 	// note that '01' (binary notation) is 1. used for matching lengths
 	let WEIGHTS = // times[x][y]
 		[ // 2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12
